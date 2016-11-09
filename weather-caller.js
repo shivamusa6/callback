9b60463a44bc3d42b0bd67a832d992fd7443abfb
SHIVAM AGRAWAL <ibmasiatraining@gmail.com>
2016-11-09 06:07:06 -0600

    done6

--- weather-caller.js
+++ weather-caller.js
@@ -1,13 +1,15 @@
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 8080);
+var http = require('http');
+var weather = require('./weather');
 var server = http.createServer( function(request, response) {
-var temp_f = '';
-weather.current(temp_f);
-var body = 'The current weather reading is ' + temp_f +
-' degrees Fahrenheit';
+weather.current( function(temp_f) {
 response.writeHead( 200, {
-'Content-Length': body.length,
 'Content-Type': 'text/plain'
 });
-response.write(body);
+response.write('The current weather reading is ' + temp_f +
+' degrees Fahrenheit');
 response.end();
+});
 });
 server.listen(port, host);
